40bcd55b5be740eb1932eb9d40627730a52a2a75
black996 <rharasis@yahoo.com>
2022-07-29 22:45:30 +0300

    add user table files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_28_192044) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_29_190301) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.boolean "done"
@@ -25,6 +25,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_28_192044) do
     t.string "created_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "items", "todos"
