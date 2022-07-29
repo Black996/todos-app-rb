7d11d543bca065eb13f2bc1d8fe26c772b2e96c9
black996 <rharasis@yahoo.com>
2022-07-29 21:29:33 +0300

    add resources to routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :todos do
+    resources :items
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
