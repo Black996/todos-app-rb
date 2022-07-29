2b6b073ec826efebad5688ad45f1a83ae40eea3f
black996 <rharasis@yahoo.com>
2022-07-29 21:32:18 +0300

    fix generated test data deprecation

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::StarWars.character }
-    done false
-    todo_id nil
+    name { Faker::Name.unique.name}
+    done {false}
+    todo_id {nil}
   end
 end
